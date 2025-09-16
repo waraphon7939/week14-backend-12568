add14305901977785ca7406e8012e99e31587311
Wara Phon <waraphon7939@gmail.com>
2025-09-16 11:46:44 +0700

    update

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
@@ -16,7 +16,7 @@ class AuthController extends Controller
             'first_name' => 'required|string|min:1',
             'last_name' => 'required|string|min:1',
             'email' => 'required|string|email|min:4|max:255|unique:users',
-            'password' => 'required|string|min:8',
+            'password' => 'required|string|min:4',
             'confirm_password' => 'required|same:password',
 
         ]);
@@ -33,5 +33,75 @@ class AuthController extends Controller
             'message' => 'User registered successfully!.',
             'user' => $user
         ], status: 201);
+    }
+
+    public function login(Request $request)
+    {
+        $credentials = $request->validate([
+            'email'    => 'required|email',
+            'password' => 'required|string',
+        ]);
+
+        if (!Auth::attempt($credentials)) {
+            return response()->json([
+                'status' => false,
+                'message' => 'Email and Password doe not match',
+                'errors' => ['Unauthorized']
+            ], 401);
+        }
+
+        $user = $request->user();
+        $user->tokens()->delete();
+        $token = $user->createToken('access_token', ['user'])->plainTextToken;
+
+        return response()->json([
+            'status' => true,
+            'message' => 'User logged in successfully!',
+            'user_info'     => [
+                'id'    => $user->id,
+                'first_name'  => $user->first_name,
+                'last_name'  => $user->last_name,
+                'email' => $user->email,
+            ],
+            'token' => $token,
+            'token_type'  => 'Bearer',
+        ], 200);
+    }
+
+    public function logout(Request $request)
+    {
+        $user = $request->user();
+        if ($user) {
+            $user->tokens()->delete();
+            return response()->json([
+                'status' => true,
+                'message' => 'Successfully logged out',
+                'data' => []
+            ], 200);
+        }
+
+        return response()->json([
+            'status' => false,
+            'message' => 'User not authenticated',
+            'errors' => ['Unauthorized']
+        ], 401);
+    }
+
+    public function profile(Request $request)
+    {
+        $user = $request->user();
+        if ($user) {
+            return response()->json([
+                'status' => true,
+                'message' => 'Profile Information',
+                'user' => $user
+            ],200);
+        }
+
+        return response()->json([
+            'status' => false,
+            'message' => 'User not authenticated',
+            'errors' => 'Unauthorized'
+        ],401);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -8,4 +8,11 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::post('{register}', [AuthController::class, 'register'])->name('register');
+Route::post('register', [AuthController::class, 'register'])->name('register');
+Route::post('login', [AuthController::class, 'login'])->name('login');
+
+
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::post('logout', [AuthController::class, 'logout'])->name('logout');
+    Route::get('profile', [AuthController::class, 'profile'])->name('profile');
+});
